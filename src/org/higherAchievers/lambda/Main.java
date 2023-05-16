a4a4f123fcf2ff985776239d51ffb0ebd770920e
Moses Hunsu <hunsumoses7@gmail.com>
2023-05-16 11:26:17 +0100

    Worked on the basic of lambda expressions and consumer functional interface

---
+++ src/org/higherAchievers/lambda/Main.java
@@ -0,0 +1,24 @@
+package org.higherAchievers.lambda;
+
+import java.util.Arrays;
+
+public class Main {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int [] numbs = {3,2,4};
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15}, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3,3}, 6)));
+    }
+
+}
---
+++ src/org/higherAchievers/lambda/lambdaIntro/HelloWorldLambda.java
@@ -0,0 +1,10 @@
+package org.higherAchievers.lambda.lambdaIntro;
+
+public class HelloWorldLambda {
+    public static void main(String[] args) {
+        //implementing sayHelloWorld using lambda
+        HelloWorldInterface helloWorldInterface = () -> "Hello World";
+
+        System.out.println(helloWorldInterface.sayHelloWorld());
+    }
+}
