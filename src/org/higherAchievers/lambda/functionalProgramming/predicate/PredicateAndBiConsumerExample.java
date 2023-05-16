370ee97cdcf8d4301ea0730c971ff23d1f17979a
Moses Hunsu <hunsumoses7@gmail.com>
2023-05-16 16:44:55 +0100

    added Predicate lambda-type examples

---
+++ src/org/higherAchievers/lambda/functionalProgramming/predicate/PredicateAndBiConsumerExample.java
@@ -0,0 +1,32 @@
+package org.higherAchievers.lambda.functionalProgramming.predicate;
+
+import org.higherAchievers.lambda.functionalProgramming.referenceClass.Instructor;
+import org.higherAchievers.lambda.functionalProgramming.referenceClass.Instructors;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+
+public class PredicateAndBiConsumerExample {
+
+    public static void main(String[] args) {
+        List<Instructor> instructors = Instructors.getAll();
+
+        // all instructor who teaches online
+        Predicate<Instructor> p1 = (i) -> i.isOnlineCourses() == true;
+
+        //instructor experience is >10
+        Predicate<Instructor> p2 = (i) -> i.getYearsOfExperience() > 10;
+
+        //BiConsumer print names and courses
+        BiConsumer<String, List<String>> biConsumer1 = (name, courses) ->
+                System.out.println("name is: " + name + " courses: " + courses);
+
+        instructors.forEach(instructor -> {
+            if (p1.and(p2).test(instructor)) {
+                biConsumer1.accept(instructor.getName(), instructor.getCourses());
+            }
+        });
+
+    }
+}
